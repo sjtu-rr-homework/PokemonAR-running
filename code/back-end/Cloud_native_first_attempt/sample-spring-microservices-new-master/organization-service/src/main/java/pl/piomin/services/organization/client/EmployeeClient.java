52b5a540f35e90ed505a1ae1358dfb8012ebb52d
macoredroid <coredroid0401@gmail.com>
2019-07-05 16:09:46 +0800

    add md

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/organization-service/src/main/java/pl/piomin/services/organization/client/EmployeeClient.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/organization-service/src/main/java/pl/piomin/services/organization/client/EmployeeClient.java
@@ -6,12 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import pl.piomin.services.organization.fallback.EmployeeClientHystrix;
 import pl.piomin.services.organization.model.Employee;
 
-@FeignClient(name = "employee-service")
+@FeignClient(name = "employee-service",fallback = EmployeeClientHystrix.class)
 public interface EmployeeClient {
 
 	@GetMapping("/organization/{organizationId}")
 	List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId);
-	
+
 }
