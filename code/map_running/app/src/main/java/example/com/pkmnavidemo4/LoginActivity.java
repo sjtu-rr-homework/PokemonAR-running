329902cff34bcdbfbc6bd49b70635a813e389a1c
GUXUER <fl632219070705@qq.com>
2019-07-11 19:06:25 +0800

    将登陆功能修改至可用，http请求已能正常实现

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/LoginActivity.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/LoginActivity.java
@@ -3,31 +3,99 @@ package example.com.pkmnavidemo4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity  extends AppCompatActivity {
     private Button login;
-    private TextView toRegister;
+    TextView textView;
+    EditText usernameText;
+    EditText passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameText=(EditText)findViewById(R.id.name);
+        passwordText=(EditText)findViewById(R.id.password);
+        textView= (TextView) findViewById(R.id.textView);
         login = (Button)findViewById(R.id.act_login_button_login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                String username=usernameText.getText().toString();
+                String password=passwordText.getText().toString();
+                login(username,password);
+                //Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                //getwebinfo();
+                //doGet("http://5e9ca34b.ngrok.io/user/login/username/macoredroid/password/c7o2r1e4");
+                //startActivity(intent);
             }
         });
-        toRegister=findViewById(R.id.act_login_text_new);
-        toRegister.setOnClickListener(new View.OnClickListener() {
+    }
+    private void login(String username,String password) {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
+            public void run() {
+                Log.d("haha","go1");
+                HttpURLConnection conn=null;
+                BufferedReader br=null;
+                String loginUrl="https://5184c2d6.ngrok.io/user/login/username/"+username+"/password/"+password;
+                try {
+                    //URL url=new URL("https://5184c2d6.ngrok.io/user/login/username/macoredroid/password/c7o2r1e4");
+                    URL url=new URL(loginUrl);
+                    conn= (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(8000);
+                    conn.setReadTimeout(8000);
+                    InputStream in=conn.getInputStream();
+                    br=new BufferedReader(new InputStreamReader(in));
+
+                    StringBuilder sb=new StringBuilder();
+                    String s;
+                    while((s = br.readLine())!=null){
+                        sb.append(s);
+                    }
+                    setContent(sb.toString());
+                    Log.d("123","---"+sb.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d("haha",e.getMessage());
+                }finally {
+                    if (conn!=null){
+                        conn.disconnect();
+                    }
+                    if (br!=null){
+                        try {
+                            br.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                }
+            }
+        }).start();
+
+    }
+
+    public void setContent(final String s) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(s);
+                Log.d("haha",s);
             }
         });
     }
