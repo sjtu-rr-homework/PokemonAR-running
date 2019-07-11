33f0d9445be9350c936b72f869cc004cb259db92
ashibatian <2318590073@sjtu.edu.cn>
2019-07-11 09:53:18 +0800

    Add: 注册界面

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/LoginActivity.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/LoginActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity  extends AppCompatActivity {
     private Button login;
+    private TextView toRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,14 @@ public class LoginActivity  extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        toRegister=findViewById(R.id.act_login_text_new);
+        toRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(intent);
             }
         });
