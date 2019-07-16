5d9c0b458fde6939b974f1ab8b33f5d2330ed0bf
GUXUER <fl632219070705@qq.com>
2019-07-16 21:42:44 +0800

    解决了之前版本定位时频繁报key错误而导致的定位效率不高问题

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/ElfDetailsActivity.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/ElfDetailsActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
-public class ElfDetailsActivity extends AppCompatActivity {
+public class  ElfDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
