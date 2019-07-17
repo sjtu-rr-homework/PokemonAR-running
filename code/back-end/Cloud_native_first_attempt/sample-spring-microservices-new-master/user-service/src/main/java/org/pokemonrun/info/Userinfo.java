352a51c34575e1c805d96aec03399f812ac28524
macoredroid <coredroid0401@gmail.com>
2019-07-17 17:28:25 +0800

    add user exp

---
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/info/Userinfo.java
@@ -0,0 +1,15 @@
+package org.pokemonrun.info;
+
+public class Userinfo {
+    public String username;
+    public int star;
+    public String email;
+    public int exp;
+    public Userinfo(String username,int star,String email,int exp)
+    {
+        this.username=username;
+        this.email=email;
+        this.star=star;
+        this.exp=exp;
+    }
+}
---
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/service/GetUserInfo.java
@@ -0,0 +1,7 @@
+package org.pokemonrun.service;
+
+import org.pokemonrun.info.Userinfo;
+
+public interface GetUserInfo {
+    public Userinfo getUserInfo(String username);
+}
