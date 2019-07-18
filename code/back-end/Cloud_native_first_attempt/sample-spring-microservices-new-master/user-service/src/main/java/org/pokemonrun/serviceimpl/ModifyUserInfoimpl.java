3d0f3f1b6c9ca01422a8fcb2c15db1920b00f039
macoredroid <coredroid0401@gmail.com>
2019-07-18 10:33:30 +0800

    finish friend

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/serviceimpl/ModifyUserInfoimpl.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/serviceimpl/ModifyUserInfoimpl.java
@@ -6,6 +6,8 @@ import org.pokemonrun.service.ModifyUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class ModifyUserInfoimpl implements ModifyUserInfo {
     @Autowired
@@ -29,5 +31,39 @@ public class ModifyUserInfoimpl implements ModifyUserInfo {
             UserDao.save(temp);
         }
         return true;
+    }
+
+    @Override
+    public boolean addFriend(String username, String friendname) {
+        User temp1=UserDao.findOne(username);
+        User temp2=UserDao.findOne(friendname);
+        if(temp1==null||temp2==null)
+        {
+            return false;
+        }
+        else
+        {
+            Set<User> follower1=temp1.getFollowers();
+            follower1.add(temp2);
+            temp1.setFollowers(follower1);
+
+            Set<User> following1=temp1.getFollowing();
+            following1.add(temp2);
+            temp1.setFollowing(following1);
+
+            UserDao.save(temp1);
+
+            Set<User> follower2=temp2.getFollowers();
+            follower2.add(temp1);
+            temp2.setFollowers(follower2);
+
+
+            Set<User> following2=temp2.getFollowing();
+            following2.add(temp1);
+            temp2.setFollowing(following2);
+
+            UserDao.save(temp2);
+        }
+        return false;
     }
 }
