662ffb8b8c1ee8fba6b58ace67da77047e295b8e
GUXUER <fl632219070705@qq.com>
2019-07-19 14:36:44 +0800

    Revert "不知道什么情况的修复"
    This reverts commit 047b036429aef4ec3429aa107edf217d19fc9669.

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/Fragments/MyFragment.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/Fragments/MyFragment.java
@@ -1,14 +1,12 @@
 package example.com.pkmnavidemo4.Fragments;
 
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
-import example.com.pkmnavidemo4.FriendPageActivity;
 import example.com.pkmnavidemo4.R;
 
 /**
@@ -16,20 +14,19 @@ import example.com.pkmnavidemo4.R;
  */
 public class MyFragment extends Fragment {
 
-    private Button checkfriend;
+    private String content;
+    public MyFragment() {
+        this.content = "nothing here";
+    }
+    public MyFragment(String content) {
+        this.content = content;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fg_content,container,false);
-        checkfriend=(Button)view.findViewById(R.id.fg_button);
-        checkfriend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //HttpHandler.getFriend();
-                Intent intent=new Intent(getActivity(), FriendPageActivity.class);
-                startActivity(intent);
-            }
-        });
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
         return view;
     }
 }
