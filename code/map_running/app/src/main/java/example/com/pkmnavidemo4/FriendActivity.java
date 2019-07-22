345a70959199d063b2a2d5228988ca4ec74d85bd
GUXUER <fl632219070705@qq.com>
2019-07-22 10:17:40 +0800

    实现查看好友信息功能，优化好友界面

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/FriendActivity.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/FriendActivity.java
@@ -3,6 +3,7 @@ package example.com.pkmnavidemo4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,17 +18,32 @@ public class FriendActivity extends AppCompatActivity {
     TextView level;
     TextView fightPoint;
     Button addfriend;
+    Button fightfriend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
         Intent intent=getIntent();
+        int type=intent.getIntExtra("type",-1);//type为1则是添加好友，type为2则是好友对战
         String friendname=intent.getStringExtra("username");
         username=(TextView)findViewById(R.id.act_friend_username);
         elfname=(TextView)findViewById(R.id.act_friend_elfname);
         level=(TextView)findViewById(R.id.act_friend_elflevel);
         fightPoint=(TextView)findViewById(R.id.act_friend_fightpoint);
         addfriend=(Button)findViewById(R.id.act_friend_add);
+        fightfriend=(Button)findViewById(R.id.act_friend_fight);
+        if(type==1){
+            addfriend.setVisibility(View.VISIBLE);
+            fightfriend.setVisibility(View.INVISIBLE);
+        }else if(type==2){
+            addfriend.setVisibility(View.INVISIBLE);
+            fightfriend.setVisibility(View.VISIBLE);
+        }else{
+            Log.d("error","type错误");
+        }
+
+        //http请求添加在这里
+
         username.setText(friendname);
         addfriend.setOnClickListener(new View.OnClickListener() {
             @Override
