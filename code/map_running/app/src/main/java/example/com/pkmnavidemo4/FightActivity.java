7c6e8df99c5919243a612b4feb177ac37a339553
ashibatian <2318590073@sjtu.edu.cn>
2019-07-22 10:14:26 +0800

    Add: 精灵对战完善

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/FightActivity.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/FightActivity.java
@@ -12,8 +12,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -35,8 +37,6 @@ public class FightActivity extends AppCompatActivity {
     private ImageView image1;
     private ImageView image2;
     private List<String> list;
-    private Queue index;
-    private List<List<String>> showList;
     private Button start;
     RecyclerView mRecyclerView;
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +48,7 @@ public class FightActivity extends AppCompatActivity {
         Intent intent = getIntent();
         leftElf = intent.getIntExtra("leftElf", 1);
         rightElf = intent.getIntExtra("rightElf", 2);
-        leftPower = intent.getIntExtra("leftPower", 100);
+        leftPower = intent.getIntExtra("leftPower", 120);
         rightPower = intent.getIntExtra("rightPower", 100);
         leftGrade = intent.getIntExtra("leftGrade",2);
         rightGrade = intent.getIntExtra("rightGrade", 2);
@@ -56,7 +56,7 @@ public class FightActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                initData();
+                startFight();
             }
         });
         image1=findViewById(R.id.act_fight_elf_player1);
@@ -64,23 +64,84 @@ public class FightActivity extends AppCompatActivity {
         image2=findViewById(R.id.act_fight_elf_player2);
         image2.setBackgroundResource(ElfSourceController.getBackgroundWithLevel(rightElf,rightGrade));
     }
-    private void initData() {
+    private void startFight() {
         list = new ArrayList<>();
-        showList=new ArrayList<>();
-        for (i = 1; i<= 20; i++) {
-
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    //do something
-                    list.add(""+i);
-                    index.add(i);
-                    showList.add(list);
-                    FightTextAdapter adapter = new FightTextAdapter(FightActivity.this,showList.get((int)index.poll()));
-                    mRecyclerView.setAdapter(adapter);
-                              }
-                  }, 500*i);
-            //实例化并传输数据给adapter
+        int attack=0;
+        //判断精灵攻击强度
+        Random luck=new Random();
+        for (i = 0; leftHp>0&&rightHp>0; i++) {
+            if(i%2==0) {
+                int randomAttack=luck.nextInt(10);
+                attack=ElfSourceController.getAttack(leftPower,rightPower)/2+luck.nextInt(ElfSourceController.getAttack(leftPower,rightPower)/2);
+                switch (randomAttack){
+                    case 1:
+                        list.add(ElfSourceController.getName(leftElf, leftGrade)+"的攻击MISS了");
+                        attack=0;
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        list.add(ElfSourceController.getName(leftElf, leftGrade)+"打出了普通一击");
+                        break;
+                    case 8:
+                    case 9:
+                        list.add(ElfSourceController.getName(leftElf, leftGrade)+"打出了会心一击");
+                        attack*=2;
+                        break;
+                    case 0:
+                        list.add(ElfSourceController.getName(leftElf, leftGrade)+"打出了致命一击");
+                        attack*=5;
+                        break;
+                    default:
+                }
+                list.add("" + ElfSourceController.getName(leftElf, leftGrade) + "对" + ElfSourceController.getName(rightElf, rightGrade) + "造成了" +attack+ "%血量的伤害" );
+                rightHp-=attack;
+                if(rightHp<=0){
+                    list.add(ElfSourceController.getName(leftElf, leftGrade)+"胜利!!!");
+                    break;
+                }
+                list.add(ElfSourceController.getName(rightElf, rightGrade) +"还有" + rightHp+ "%血量");
+            }
+            else {
+                attack=ElfSourceController.getAttack(rightPower,leftPower)/2+luck.nextInt(ElfSourceController.getAttack(rightPower,leftPower)/2);
+                int randomAttack=luck.nextInt(10);
+                switch (randomAttack){
+                    case 1:
+                        list.add("" +ElfSourceController.getName(rightElf, rightGrade)+"的攻击MISS了");
+                        attack=0;
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        list.add("" +ElfSourceController.getName(rightElf, rightGrade)+"打出了普通一击");
+                        break;
+                    case 8:
+                    case 9:
+                        list.add("" +ElfSourceController.getName(rightElf, rightGrade)+"打出了会心一击");
+                        attack*=2;
+                        break;
+                    case 0:
+                        list.add("" +ElfSourceController.getName(rightElf, rightGrade)+"打出了致命一击");
+                        attack*=5;
+                        break;
+                    default:
+                }
+                list.add("     " + "" + ElfSourceController.getName(rightElf, rightGrade) + "对" + ElfSourceController.getName(leftElf, leftGrade) + "造成了" + attack + "%血量的伤害" );
+                leftHp -=attack;
+                if(leftHp<=0){
+                    list.add(ElfSourceController.getName(rightElf, rightGrade)+"胜利!!!");
+                    break;
+                }
+                list.add(ElfSourceController.getName(leftElf, leftGrade) +"还有" + leftHp+ "%血量");
+            }
         }
+            FightTextAdapter adapter = new FightTextAdapter(FightActivity.this,list);
+            mRecyclerView.setAdapter(adapter);
     }
 }
