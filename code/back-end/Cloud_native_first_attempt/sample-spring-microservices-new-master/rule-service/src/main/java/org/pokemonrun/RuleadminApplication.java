8564e4da7ed96364b45b3a9f46e055eb1c89163f
macoredroid <coredroid0401@gmail.com>
2019-07-23 09:35:04 +0800

    enable swagger on rule

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/RuleadminApplication.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/RuleadminApplication.java
@@ -2,8 +2,12 @@ package org.pokemonrun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
 public class RuleadminApplication {
 
     public static void main(String[] args) {
