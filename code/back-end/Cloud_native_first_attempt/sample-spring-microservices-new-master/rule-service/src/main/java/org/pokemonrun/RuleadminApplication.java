c4a1416cae01931c0c16169512ef72fa856ced65
macoredroid <coredroid0401@gmail.com>
2019-07-26 11:13:36 +0800

    add @Config to Application

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/RuleadminApplication.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/RuleadminApplication.java
@@ -1,13 +1,17 @@
 package org.pokemonrun;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
+@Configuration
 public class RuleadminApplication {
 
     public static void main(String[] args) {
--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/config/WebSecurityConfig.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/config/WebSecurityConfig.java
@@ -2,8 +2,9 @@ package org.pokemonrun.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
