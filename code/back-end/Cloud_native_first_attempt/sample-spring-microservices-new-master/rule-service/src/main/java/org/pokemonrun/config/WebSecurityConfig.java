4829bcccb9fdd9b964b4696453591c24175fd276
macoredroid <coredroid0401@gmail.com>
2019-07-26 11:22:19 +0800

    permit all request

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/config/WebSecurityConfig.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/config/WebSecurityConfig.java
@@ -10,6 +10,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
     }
 }
