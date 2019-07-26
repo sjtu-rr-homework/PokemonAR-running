ffa011019db44c43ac3f7947fb74e263dfaeb3a5
macoredroid <coredroid0401@gmail.com>
2019-07-26 10:56:42 +0800

    add spring security dependency for rule-service

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/config/WebSecurityConfig.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/rule-service/src/main/java/org/pokemonrun/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package org.pokemonrun.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
