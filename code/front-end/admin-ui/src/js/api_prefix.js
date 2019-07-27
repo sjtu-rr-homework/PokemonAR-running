7dd009a067e61047d900eaaa42201c5aa9aa2c11
macoredroid <coredroid0401@gmail.com>
2019-07-27 11:31:31 +0800

    modify link

--- code/front-end/admin-ui/src/js/api_prefix.js
+++ code/front-end/admin-ui/src/js/api_prefix.js
@@ -1,14 +1,14 @@
 module.exports = {
     userApi: function (url) {
-        return 'http://8db5acdd.ngrok.io/user/' + url;
+        return 'http://202.120.40.8:30751/user/' + url;
     },
     recordApi: function (url) {
-        return 'http://8db5acdd.ngrok.io/record/' + url;
+        return 'http://202.120.40.8:30751/record/' + url;
     },
     ruleApi: function (url) {
-        return 'http://8db5acdd.ngrok.io/rule/' + url;
+        return 'http://202.120.40.8:30751/rule/' + url;
     },
     petApi: function (url) {
-        return 'http://8db5acdd.ngrok.io/pet/' + url;
+        return 'http://202.120.40.8:30751/pet/' + url;
     }
 };
