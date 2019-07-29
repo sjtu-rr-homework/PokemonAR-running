a2ce1be0535e41b2a7968d4242500050d49aa3e7
macoredroid <coredroid0401@gmail.com>
2019-07-29 11:03:23 +0800

    add moment dao and service

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/dao/MomentDao.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/dao/MomentDao.java
@@ -1,4 +1,11 @@
 package org.pokemonrun.dao;
 
+import org.pokemonrun.entity.Moment;
+
+import java.util.List;
+
 public interface MomentDao {
+    List<Moment> getAll();
+    List<Moment> getUserAll(String username);
+    void save(Moment moment);
 }
--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/repository/MomentRepository.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/repository/MomentRepository.java
@@ -3,5 +3,8 @@ package org.pokemonrun.repository;
 import org.pokemonrun.entity.Moment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MomentRepository extends MongoRepository<Moment,String> {
+    List<Moment> findAllByUsername(String username);
 }
