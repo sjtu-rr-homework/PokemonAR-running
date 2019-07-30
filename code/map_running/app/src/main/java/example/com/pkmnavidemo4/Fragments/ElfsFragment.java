687956de9e1e57513dbbcdb26b499d7708b44c84
GUXUER <fl632219070705@qq.com>
2019-07-30 17:27:39 +0800

    wkysb

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/Fragments/ElfsFragment.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/Fragments/ElfsFragment.java
@@ -35,10 +35,8 @@ import example.com.pkmnavidemo4.classes.UserData;
 public class ElfsFragment extends Fragment {
     private Switch switchModel;
     private Button refresh;
-    private TextView refreshText;
     private List<String> list;
     RecyclerView mRecyclerView;
-    private boolean lock;
     public ElfsFragment() {
 
     }
@@ -55,7 +53,6 @@ public class ElfsFragment extends Fragment {
         TestRecycleViewAdapter adapter = new TestRecycleViewAdapter(getActivity(),list);
         mRecyclerView.setAdapter(adapter);
         refresh( mRecyclerView);
-        refreshText=view.findViewById(R.id.elf_fg_text_wait);
         refresh=view.findViewById(R.id.elf_fg_button_refresh);
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,13 +101,6 @@ public class ElfsFragment extends Fragment {
         mRecyclerView.setAdapter(adapter);
     }
 
-    private void setLock(){
-        lock=true;
-    }
-
-    public void unLock(){
-        lock=false;
-    }
     //得到所有精灵
     private void initData() {
         list = new ArrayList<>();
@@ -141,38 +131,4 @@ public class ElfsFragment extends Fragment {
         Log.v("dbw", "Status height:" + height);
         return height;
     }
-    /*private Button arButton;
-    private Button arButton2;
-    public ElfsFragment() {
-
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.elfs_fg_content,container,false);
-        arButton=view.findViewById(R.id.button1);
-        arButton2=view.findViewById(R.id.button2);
-        arButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SceneformActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putInt("variety", 1);
-                intent.putExtras(bundle);
-//这里一定要获取到所在Activity再startActivity()；
-                getActivity().startActivity(intent);
-            }
-        });
-        arButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SceneformActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putInt("variety", 2);
-                intent.putExtras(bundle);
-//这里一定要获取到所在Activity再startActivity()；
-                getActivity().startActivity(intent);
-            }
-        });
-        return view;
-    }*/
 }
