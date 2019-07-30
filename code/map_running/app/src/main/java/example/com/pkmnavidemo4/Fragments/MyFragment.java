3164c4f332fe096e42ef5ac616a1e28e5e2b51c6
GUXUER <fl632219070705@qq.com>
2019-07-30 11:25:17 +0800

    我的信息页面能正常显示信息（跑步里程，计入成绩，学期目标）

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/Fragments/MyFragment.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/Fragments/MyFragment.java
@@ -14,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 
+import java.text.DecimalFormat;
+
 import example.com.pkmnavidemo4.FriendPageActivity;
 import example.com.pkmnavidemo4.R;
 import example.com.pkmnavidemo4.classes.ElfSourceController;
@@ -26,11 +28,23 @@ import example.com.pkmnavidemo4.classes.UserData;
 public class MyFragment extends Fragment {
 
     private LinearLayout checkfriend;
+    private TextView distance;
+    private TextView mileage;
+    private TextView mileageGoal;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fg_content,container,false);
         checkfriend=(LinearLayout) view.findViewById(R.id.fg_layout_check_friend);
+        distance=(TextView)view.findViewById(R.id.fg_achieve);
+        mileage=(TextView)view.findViewById(R.id.fg_score);
+        mileageGoal=(TextView)view.findViewById(R.id.fg_goal);
+
+        DecimalFormat format=new DecimalFormat("#0.00");
+        distance.setText(""+format.format(UserData.distance/1000)+"公里");
+        mileage.setText(""+format.format(UserData.getMileage()/1000)+"公里");
+        mileageGoal.setText(""+format.format(UserData.getMileageGoal()/1000)+"公里");
+
         checkfriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
