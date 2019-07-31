ff5593cafc0a9a07faf74c8b370792c3fa319a33
GUXUER <fl632219070705@qq.com>
2019-07-31 15:06:57 +0800

    在登录界面加入记住密码和自动登录的选项，并加入强制逻辑：必须勾选记住密码后才能勾选自动登录

--- code/map_running/app/src/main/java/example/com/pkmnavidemo4/LoginActivity.java
+++ code/map_running/app/src/main/java/example/com/pkmnavidemo4/LoginActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,6 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import example.com.pkmnavidemo4.classes.HttpHandler;
+import example.com.pkmnavidemo4.classes.SharedPreferencesUtil;
 import example.com.pkmnavidemo4.classes.UserData;
 
 public class LoginActivity  extends AppCompatActivity {
@@ -27,25 +30,51 @@ public class LoginActivity  extends AppCompatActivity {
     EditText usernameText;
     EditText passwordText;
     TextView text;
+    CheckBox remember_password;
+    CheckBox auto_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         usernameText=(EditText)findViewById(R.id.name);
         passwordText=(EditText)findViewById(R.id.password);
+        remember_password=(CheckBox)findViewById(R.id.act_login_remember);
+        auto_login=(CheckBox)findViewById(R.id.act_login_auto);
         text=(TextView)findViewById(R.id.act_login_text_jaccount);
         login = (Button)findViewById(R.id.act_login_button_login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String username=usernameText.getText().toString();
                 String password=passwordText.getText().toString();
+                if(remember_password.isChecked()){
+                    SharedPreferencesUtil.putString(getApplicationContext(),"username",username);
+                    SharedPreferencesUtil.putString(getApplicationContext(),"password",password);
+                    SharedPreferencesUtil.putBoolean(getApplicationContext(),"isremember",true);
+                }
                 HttpHandler.login(LoginActivity.this,username,password);
                 HttpHandler.getMileage(username);
                 HttpHandler.getElfs(username);
                 HttpHandler.getExp(username);
                 UserData.setUserInfo(username,1);
                 UserData.initonlyHave();
+            }
+        });
+        auto_login.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!remember_password.isChecked()&&b){
+                    auto_login.setChecked(false);
+                }
+            }
+        });
+        remember_password.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!b&&auto_login.isChecked()){
+                    auto_login.setChecked(false);
+                }
             }
         });
         toRegister=findViewById(R.id.act_login_text_new);
