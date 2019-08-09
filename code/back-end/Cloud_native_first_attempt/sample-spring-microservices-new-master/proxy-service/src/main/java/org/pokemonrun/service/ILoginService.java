254070d58582047d902125c3fec674683ace2562
macoredroid <coredroid0401@gmail.com>
2019-08-09 08:15:29 +0800

    add login service

---
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/proxy-service/src/main/java/org/pokemonrun/service/ILoginService.java
@@ -0,0 +1,14 @@
+package org.pokemonrun.service;
+
+import com.arjun.gateway.bean.auth.User;
+
+public interface ILoginService {
+    String login(String username, String password);
+    User saveUser(User user);
+
+    boolean logout(String token);
+
+    Boolean isValidToken(String token);
+
+    String createNewToken(String token);
+}
