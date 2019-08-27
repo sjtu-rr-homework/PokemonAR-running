16d33f5d4900d06bb078bf92262e5189b02e4c1c
macoredroid <coredroid0401@gmail.com>
2019-08-27 22:22:22 +0800

    add enetity

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/entity/FastMoment.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/entity/FastMoment.java
@@ -1,4 +1,51 @@
 package org.pokemonrun.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties(value = {"handler","hibernateLazyInitializer","fieldHandler"})
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "momentid")
 public class FastMoment {
+    private String momentid;
+    private long timestamp;
+    @Id
+    @Column(name = "momentid")
+    public String getMomentid()
+    {
+        return momentid;
+    }
+    public void setMomentid()
+    {
+        this.momentid=momentid;
+    }
+
+    @Column(name = "timestamp")
+    public long getTimestamp()
+    {
+        return timestamp;
+    }
+    public void setTimestamp()
+    {
+        this.timestamp=timestamp;
+    }
+
+    public FastMoment()
+    {
+
+    }
+
+    public FastMoment(String momentid, long timestamp)
+    {
+        this.momentid=momentid;
+        this.timestamp=timestamp;
+    }
+
 }
