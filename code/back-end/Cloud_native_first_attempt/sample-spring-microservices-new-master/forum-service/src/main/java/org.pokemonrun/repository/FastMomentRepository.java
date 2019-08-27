b2551d62166f8f44ef65a337beb70962a7bbbf88
macoredroid <coredroid0401@gmail.com>
2019-08-27 23:17:55 +0800

    add repo

---
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/repository/FastMomentRepository.java
@@ -0,0 +1,9 @@
+package org.pokemonrun.repository;
+
+import org.pokemonrun.entity.FastMoment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FastMomentRepository extends JpaRepository<FastMoment, String> {
+
+
+}
