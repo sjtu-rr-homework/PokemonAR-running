134920597f66fea0b5eeecc450057589c0780205
macoredroid <coredroid0401@gmail.com>
2019-08-28 02:13:35 +0800

    edit repo

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/repository/MomentRepository.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/repository/MomentRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface MomentRepository extends MongoRepository<Moment,String> {
     List<Moment> findAllByUsername(String username);
+    List<Moment> findTop10ByTimestampLessThanOrderBytOrderByTimestampAsc(long timestamp);
+    List<Moment> findTop10ByTimestampGreaterThanOrderByTimestampDesc(long timestamp);
 }
