42a2274bfc08f0f6b8ed58504d0c164d5fa7b933
macoredroid <coredroid0401@gmail.com>
2019-08-28 23:09:09 +0800

    open oauth

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/proxy-service/src/main/java/org/pokemonrun/security/WebSecurityConfig.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/proxy-service/src/main/java/org/pokemonrun/security/WebSecurityConfig.java
@@ -32,7 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // Entry points
         http.authorizeRequests()
                 .antMatchers("/**/signin/**").permitAll()
-                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
         // Apply JWT
@@ -45,7 +44,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         // Allow eureka client to be accessed without authentication
-        web.ignoring().antMatchers("/eureka/**")
+        web.ignoring()
+                .antMatchers("/eureka/**")
                 .antMatchers("/**/register/**")
                 .antMatchers(HttpMethod.OPTIONS, "/**"); // Request type options should be allowed.
 
