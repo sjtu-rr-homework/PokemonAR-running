f2d6055c5645dfec32eee09e2c8d8fa36a5b8d39
macoredroid <coredroid0401@gmail.com>
2019-09-02 09:34:01 +0800

    fix cors

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/proxy-service/src/main/java/org/pokemonrun/security/WebSecurityConfig.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/proxy-service/src/main/java/org/pokemonrun/security/WebSecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -56,6 +59,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**/register/**")
                 .antMatchers(HttpMethod.OPTIONS, "/**"); // Request type options should be allowed.
 
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
