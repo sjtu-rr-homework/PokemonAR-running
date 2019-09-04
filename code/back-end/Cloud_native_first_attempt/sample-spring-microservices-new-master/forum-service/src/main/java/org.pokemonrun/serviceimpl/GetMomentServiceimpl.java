fbb14f322e04096553400f3c3f32f08b532b4de9
macoredroid <coredroid0401@gmail.com>
2019-09-04 17:27:00 +0800

    add comment

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/serviceimpl/GetMomentServiceimpl.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/forum-service/src/main/java/org.pokemonrun/serviceimpl/GetMomentServiceimpl.java
@@ -33,11 +33,6 @@ public class GetMomentServiceimpl implements GetMomentService {
                     MomentInfo tempInfo = new MomentInfo(tempMoment.text, tempMoment.timestamp, tempMoment.username, tempMoment.pic1, tempMoment.pic2, tempMoment.pic3, tempMoment.pic4, tempMoment.pic5, tempMoment.pic6, tempMoment.pic7, tempMoment.pic8, tempMoment.pic9, UserClient.getCover(tempMoment.username).pic);
                     tempInfoList.add(tempInfo);
                 }
-                else
-                {
-                    MomentInfo tempInfo = new MomentInfo(tempMoment.text, tempMoment.timestamp, tempMoment.username, tempMoment.pic1, tempMoment.pic2, tempMoment.pic3, tempMoment.pic4, tempMoment.pic5, tempMoment.pic6, tempMoment.pic7, tempMoment.pic8, tempMoment.pic9, null);
-                    tempInfoList.add(tempInfo);
-                }
             }
 
         }
@@ -56,11 +51,6 @@ public class GetMomentServiceimpl implements GetMomentService {
                 if(UserClient.getCover(tempMoment.username)!=null)
                 {
                     MomentInfo tempInfo = new MomentInfo(tempMoment.text, tempMoment.timestamp, tempMoment.username, tempMoment.pic1, tempMoment.pic2, tempMoment.pic3, tempMoment.pic4, tempMoment.pic5, tempMoment.pic6, tempMoment.pic7, tempMoment.pic8, tempMoment.pic9, UserClient.getCover(tempMoment.username).pic);
-                    tempInfoList.add(tempInfo);
-                }
-                else
-                {
-                    MomentInfo tempInfo = new MomentInfo(tempMoment.text, tempMoment.timestamp, tempMoment.username, tempMoment.pic1, tempMoment.pic2, tempMoment.pic3, tempMoment.pic4, tempMoment.pic5, tempMoment.pic6, tempMoment.pic7, tempMoment.pic8, tempMoment.pic9, null);
                     tempInfoList.add(tempInfo);
                 }
             }
