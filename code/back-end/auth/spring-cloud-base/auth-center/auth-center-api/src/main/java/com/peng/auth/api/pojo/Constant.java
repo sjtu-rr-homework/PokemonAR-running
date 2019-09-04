39bf42a94389205cd34c2c01d0828c164460ce60
macoredroid <coredroid0401@gmail.com>
2019-09-04 15:53:03 +0800

    update doc

--- code/back-end/auth/spring-cloud-base/auth-center/auth-center-api/src/main/java/com/peng/auth/api/pojo/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.peng.auth.api.pojo;
-
-/**
- * Created by fp295 on 2018/4/16.
- */
-public class Constant {
-
-    public static String USER_INFO = "user_info";
-}
--- code/back-end/auth/spring-cloud-base/web-app/src/plugin/auth-client/utils/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  appId: null,
-  appSecret: null,
-  authUrl: null,
-  baseUrl: null,
-  grant_type: 'authorization_code'
-}
