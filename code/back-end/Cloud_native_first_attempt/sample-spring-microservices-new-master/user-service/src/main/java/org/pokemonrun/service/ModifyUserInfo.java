97c18e544035e1d26c6916ca4c7ffdc386b751c8
macoredroid <coredroid0401@gmail.com>
2019-09-06 12:22:23 +0800

    add comment

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/service/ModifyUserInfo.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/service/ModifyUserInfo.java
@@ -6,7 +6,7 @@ public interface ModifyUserInfo {
     public boolean ModifyExp(String username,int num);
     public boolean blockUser(String username);//block user from login (set star)
     public boolean SetPet(String username, int pet);//user set the pet to fight
-    public boolean AddDistance(String username, double distance);
-    public boolean addFriend(String username,String friendname);
+    public boolean AddDistance(String username, double distance);//add to the whole distance a user has run
+    public boolean addFriend(String username,String friendname);//add a user as another user's friend, automatically back and forth
     public boolean addCover(Coverinfo Coverinfo);
 }
