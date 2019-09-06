2679285ef2069261058c1edd2e34c7793c8e0009
macoredroid <coredroid0401@gmail.com>
2019-09-06 12:05:33 +0800

    add comment

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/dao/CoverDao.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/dao/CoverDao.java
@@ -3,6 +3,6 @@ package org.pokemonrun.dao;
 import org.pokemonrun.entity.Cover;
 
 public interface CoverDao {
-    Cover getOneCover(String username);
-    void save(Cover cover);
+    Cover getOneCover(String username);//get avatar
+    void save(Cover cover);//set avatar
 }
--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/service/ModifyUserInfo.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/user-service/src/main/java/org/pokemonrun/service/ModifyUserInfo.java
@@ -4,8 +4,8 @@ import org.pokemonrun.info.Coverinfo;
 
 public interface ModifyUserInfo {
     public boolean ModifyExp(String username,int num);
-    public boolean blockUser(String username);
-    public boolean SetPet(String username, int pet);
+    public boolean blockUser(String username);//block user from login (set star)
+    public boolean SetPet(String username, int pet);//user set the pet to fight
     public boolean AddDistance(String username, double distance);
     public boolean addFriend(String username,String friendname);
     public boolean addCover(Coverinfo Coverinfo);
