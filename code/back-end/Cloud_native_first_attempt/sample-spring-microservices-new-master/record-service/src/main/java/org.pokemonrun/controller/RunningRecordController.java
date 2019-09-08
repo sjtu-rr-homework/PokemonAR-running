96f9e31212f3b114d6f657fa54251b370b348144
84461810 <431600414@qq.com>
2019-09-08 18:37:56 +0800

    Add record service comments

--- code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/record-service/src/main/java/org.pokemonrun/controller/RunningRecordController.java
+++ code/back-end/Cloud_native_first_attempt/sample-spring-microservices-new-master/record-service/src/main/java/org.pokemonrun/controller/RunningRecordController.java
@@ -13,16 +13,33 @@ public class RunningRecordController {
     @Autowired
     private RecordService recordService;
 
+    /**
+     * Upload a running record.
+     * @param runningRecordInfo a RunningRecordInfo object describing the running record
+     * @return whether the running record is saved successfully
+     */
     @PostMapping("/running/record")
     public boolean saveRecord(@RequestBody RunningRecordInfo runningRecordInfo) {
         return recordService.saveRecord(runningRecordInfo);
     }
 
+    /**
+     * Get a user's running records.
+     * @param username the username of user
+     * @return a list containing all the running records of the user
+     */
     @GetMapping("/running/record/user/{username}")
     public List<RunningRecordInfo> getUserRecordList(@PathVariable("username") String username) {
         return recordService.getUserRecordList(username);
     }
 
+    /**
+     * Get a user's running records by page.
+     * At most 10 records per page.
+     * @param username the username of user
+     * @param pid page index
+     * @return a page of user's running records
+     */
     @GetMapping("/running/record/user/{username}/page/{pageid}")
     public List<RunningRecordInfo> getUserRecordList(@PathVariable("username") String username,@PathVariable("pageid") String pid) {
         int pageID = Integer.parseInt(pid);
